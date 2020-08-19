aa9f8d40866e399b80828de8666851f0bb82360f
nkumar-topcoder <33625707+nkumar-topcoder@users.noreply.github.com>
2020-08-19 09:04:09 +0530

    Update run-service.sh

--- src/main/docker/run-service.sh
+++ src/main/docker/run-service.sh
@@ -3,7 +3,8 @@
 
 if [ "$AP_ENV" = "prod" ]
 then
-	java -Dnewrelic.environment=$AP_ENV -javaagent:/data/dd-java-agent.jar -Ddd.agent.host=$DD_TRACE_AGENT_HOSTNAME -Ddd.agent.port=8126 -Ddd.service.name=$DD_SERVICE_NAME -javaagent:$NEWRELIC_JAR -Djavax.net.ssl.trustStore=/data/TC.prod.ldap.keystore -DZOOKEEPER_HOSTS_LIST=$ZOOKEEPER_HOSTS_LIST -jar /data/tech.core.service.identity.jar server /data/config.yml
+	#java -Dnewrelic.environment=$AP_ENV -javaagent:/data/dd-java-agent.jar -Ddd.agent.host=$DD_TRACE_AGENT_HOSTNAME -Ddd.agent.port=8126 -Ddd.service.name=$DD_SERVICE_NAME -javaagent:$NEWRELIC_JAR -Djavax.net.ssl.trustStore=/data/TC.prod.ldap.keystore -DZOOKEEPER_HOSTS_LIST=$ZOOKEEPER_HOSTS_LIST -jar /data/tech.core.service.identity.jar server /data/config.yml
+	java -Dnewrelic.environment=$AP_ENV -javaagent:$NEWRELIC_JAR -Djavax.net.ssl.trustStore=/data/TC.prod.ldap.keystore -DZOOKEEPER_HOSTS_LIST=$ZOOKEEPER_HOSTS_LIST -jar /data/tech.core.service.identity.jar server /data/config.yml
 else
 	#java -javaagent:/data/dd-java-agent.jar -Ddd.agent.host=$DD_TRACE_AGENT_HOSTNAME -Ddd.agent.port=8126 -Ddd.service.name=$DD_SERVICE_NAME -Djavax.net.ssl.trustStore=/data/TC.prod.ldap.keystore -DZOOKEEPER_HOSTS_LIST=$ZOOKEEPER_HOSTS_LIST -jar /data/tech.core.service.identity.jar server /data/config.yml
 	java -Djavax.net.ssl.trustStore=/data/TC.prod.ldap.keystore -DZOOKEEPER_HOSTS_LIST=$ZOOKEEPER_HOSTS_LIST -jar /data/tech.core.service.identity.jar server /data/config.yml
