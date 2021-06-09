7b06cd4ada85292eca0f11b6e48744b35fd5487d
Sachin Maheshwari <sachin.maheshwari@topcoder.com>
2021-06-09 19:29:54 +0530

    lib issue

--- src/main/java/com/appirio/tech/core/service/identity/resource/UserResource.java
+++ src/main/java/com/appirio/tech/core/service/identity/resource/UserResource.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.LinkedHashMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
