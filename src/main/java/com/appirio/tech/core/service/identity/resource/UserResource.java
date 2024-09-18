060073618f046977d026215ca7cd7ac46a9625df
Justin Gasper <jmgasper@gmail.com>
2024-09-18 11:11:16 +1000

    Better logging

--- src/main/java/com/appirio/tech/core/service/identity/resource/UserResource.java
+++ src/main/java/com/appirio/tech/core/service/identity/resource/UserResource.java
@@ -1808,7 +1808,7 @@ public class UserResource implements GetResource<User>, DDLResource<User> {
         if (response.getStatusCode() != HttpURLConnection.HTTP_OK) {
             sendSlackNotification(diceAttributes.getHandle(), "Error happened, please check the logs.");
             throw new APIRuntimeException(HttpURLConnection.HTTP_INTERNAL_ERROR,
-                    String.format("Got unexpected response from remote service. %d %s", response.getStatusCode(),
+                    String.format("Error when calling dice connection api with body. %s %s %d %s", mapper.writeValueAsString(body), token, response.getStatusCode(),
                             response.getMessage()));
         }
         DiceInvitationResponse diceInvitation = new ObjectMapper().readValue(response.getText(),
