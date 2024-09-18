27da0db67ab33b22b2f31733587ac20101878467
Justin Gasper <jmgasper@gmail.com>
2024-09-18 14:55:51 +1000

    Seeing if this helps with DiceID callback

--- src/main/java/com/appirio/tech/core/service/identity/representation/DiceStatusRequest.java
+++ src/main/java/com/appirio/tech/core/service/identity/representation/DiceStatusRequest.java
@@ -1,23 +1,21 @@
 package com.appirio.tech.core.service.identity.representation;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class DiceStatusRequest {
 
-	private String type;
-	private String action;
-	private String name;
-	private String email;
-    @JsonProperty(value = "connection_id")
+	private String event;
 	private String connectionId;
-	@JsonProperty(value = "credential_exchange_id")
-	private String credentialExchangeId;
-    @JsonProperty(value = "presentation_exchange_id")
-	private String presentationExchangeId;
-    @JsonProperty(value = "schema_id")
-	private String schemaId;
-	
-    public String getConnectionId() {
+	private String emailId;
+	private String shortUrl;
+
+	public String getEvent() {
+		return event;
+	}
+
+	public void setEvent(String event) {
+		this.event = event;
+	}
+
+	public String getConnectionId() {
 		return connectionId;
 	}
 
@@ -25,59 +23,19 @@ public class DiceStatusRequest {
 		this.connectionId = connectionId;
 	}
 
-	public String getCredentialExchangeId() {
-		return credentialExchangeId;
+	public String getEmailId() {
+		return emailId;
 	}
 
-	public void setCredentialExchangeId(String credentialExchangeId) {
-		this.credentialExchangeId = credentialExchangeId;
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
 	}
 
-	public String getPresentationExchangeId() {
-		return presentationExchangeId;
+	public String getShortUrl() {
+		return shortUrl;
 	}
 
-	public void setPresentationExchangeId(String presentationExchangeId) {
-		this.presentationExchangeId = presentationExchangeId;
-	}
-
-	public String getSchemaId() {
-		return schemaId;
-	}
-
-	public void setSchemaId(String schemaId) {
-		this.schemaId = schemaId;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String getAction() {
-		return action;
-	}
-
-	public void setAction(String action) {
-		this.action = action;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
+	public void setShortUrl(String shortUrl) {
+		this.shortUrl = shortUrl;
 	}
 }
