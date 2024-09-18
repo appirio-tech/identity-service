41feccd1e8cb83c3be4dd23d67fee86e6da705fa
Justin Gasper <jmgasper@gmail.com>
2024-09-18 14:15:55 +1000

    Seeing if this helps with DiceID callback

--- src/main/java/com/appirio/tech/core/service/identity/representation/DiceStatusRequest.java
+++ src/main/java/com/appirio/tech/core/service/identity/representation/DiceStatusRequest.java
@@ -1,11 +1,53 @@
 package com.appirio.tech.core.service.identity.representation;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class DiceStatusRequest {
 
 	private String type;
 	private String action;
 	private String name;
 	private String email;
+    @JsonProperty(value = "connection_id")
+	private String connectionId;
+	@JsonProperty(value = "credential_exchange_id")
+	private String credentialExchangeId;
+    @JsonProperty(value = "presentation_exchange_id")
+	private String presentationExchangeId;
+    @JsonProperty(value = "schema_id")
+	private String schemaId;
+	
+    public String getConnectionId() {
+		return connectionId;
+	}
+
+	public void setConnectionId(String connectionId) {
+		this.connectionId = connectionId;
+	}
+
+	public String getCredentialExchangeId() {
+		return credentialExchangeId;
+	}
+
+	public void setCredentialExchangeId(String credentialExchangeId) {
+		this.credentialExchangeId = credentialExchangeId;
+	}
+
+	public String getPresentationExchangeId() {
+		return presentationExchangeId;
+	}
+
+	public void setPresentationExchangeId(String presentationExchangeId) {
+		this.presentationExchangeId = presentationExchangeId;
+	}
+
+	public String getSchemaId() {
+		return schemaId;
+	}
+
+	public void setSchemaId(String schemaId) {
+		this.schemaId = schemaId;
+	}
 
 	public String getType() {
 		return type;
